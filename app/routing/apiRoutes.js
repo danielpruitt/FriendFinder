043a65a11c58643b0b2d238f4b106f87e33fbbfb
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-08-11 21:37:55 -0400

    functionality, no images

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,34 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function (req,res){
-        friends.push(req.body);
-        res.json(true);
+        // friends.push(req.body);
+        // res.json(true);
+
+        var userInput = req.body;
+        var userScores = userInput.scores;
+
+        var bestFriendName = "";
+        var bestFriendImg = "";
+
+        var totalDifference = 1000;
+
+        for (var i =0; i < friends.length; i++){
+            console.log(friends[i]);
+            //difference starts at 0 before comparing to a total
+            var diff = 0;
+            for(var j = 0; j<userScores.length;j++){
+                diff += Math.abs(friends[i].scores[j] - userScores[j]);
+            };
+
+            if (diff < totalDifference) { 
+                totalDifference = diff;
+                bestFriendName = friends[i].name;
+                bestFriendImg = friends[i].photo;
+            };
+        };
+
+        friends.push(userInput);
+        res.json({name:bestFriendName, photo:bestFriendImg});
+
     });
 };
--- server.js
+++ server.js
@@ -16,20 +16,6 @@ app.use(bodyParser.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-
-// app.get("/", function (req, res){
-//     res.sendFile(path.join(__dirname, "app/public/home.html"));
-// });
-
-// app.get("/survey", function (req, res){
-//     res.sendFile(path.join(__dirname, "app/public/survey.html"));
-// });
-
-// app.get("/api/friends", function (req, res){
-//     return res.json(path.join(__dirname, "app/routing/apiRoutes.js"));
-// });
-
-
 //Start the server listening
 app.listen(PORT, function() {
     console.log("This app is listening on PORT " + PORT);
